156c5657a2aa6312908ca74fe109783ce7fcbe43
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2020-01-31 20:54:18 +0530

    switch to postgresql

--- phix/settings.py
+++ phix/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'phix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'phix',
+        'USER': 'root',
+        'PASSWORD': 'adminr00t',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
