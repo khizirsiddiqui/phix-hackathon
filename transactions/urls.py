b82424e20304ac873053685989ae7fe2a00bae4b
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2020-02-01 10:31:36 +0530

    remove url get request

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path(r'all_transaction/', views.transaction_list, name='transaction_list'),
+    path(r'analytics/', views.get_analytics, name='analytics'),
 
-    path(r'debits/<username>/', views.user_transaction_list, name='debit_list', kwargs={'txn_type': 0}),
-    path(r'credits/<username>/', views.user_transaction_list, name='credit_list', kwargs={'txn_type': 1}),
+    path(r'debits/', views.user_transaction_list, name='debit_list', kwargs={'txn_type': 0}),
+    path(r'credits/', views.user_transaction_list, name='credit_list', kwargs={'txn_type': 1}),
 
-    path(r'today_transactions/<username>/', views.user_today_transactions, name='user_today_transactions'),
+    path(r'today_transactions/', views.user_today_transactions, name='user_today_transactions'),
     path(r'transactions/<username1>/<username2>', views.all_P2P_transactions, name='all_P2P_transactions'),
 
     path(r'create_transaction/', views.TransactionAPIView.as_view(), name='create_transaction'),
--- transactions/views.py
+++ transactions/views.py
@@ -29,17 +29,45 @@ def transaction_list(request):
         serializer = TransactionSerializer(transactions, many=True)
         return JSONResponse(serializer.data)
 
-def user_transaction_list(request, username, txn_type):
+def user_transaction_list(request, txn_type):
     if request.method == 'GET':
-        user = get_object_or_404(User, username=username)
+        if not 'username' in request.GET:
+            return JSONResponse({
+                'error_code': 'E007',
+                'error': 'Please provide a valid username'
+            })
+        username = request.GET['username']
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return JSONResponse(
+                {
+                    'error': 'User does NOT exists.',
+                    'error_code': 'E001'
+                }
+            )
         profile = Profile.objects.get(user=user)
         transactions = profile.get_all_specific(txn_type=txn_type)
         serializer = TransactionSerializer(transactions, many=True)
         return JSONResponse(serializer.data)
 
-def user_today_transactions(request, username):
+def user_today_transactions(request):
     if request.method == 'GET':
-        user = get_object_or_404(User, username=username)
+        if not 'username' in request.GET:
+            return JSONResponse({
+                'error_code': 'E007',
+                'error': 'Please provide a valid username'
+            })
+        username = request.GET['username']
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return JSONResponse(
+                {
+                    'error': 'User does NOT exists.',
+                    'error_code': 'E001'
+                }
+            )
         profile = Profile.objects.get(user=user)
         transactions = profile.get_today_transactions()
         serializer = TransactionSerializer(transactions, many=True)
@@ -53,6 +81,29 @@ def all_P2P_transactions(request, username1, username2):
         transactions = profile.all_P2P_transactions(second_user)
         serializer = TransactionSerializer(transactions, many=True)
         return JSONResponse(serializer.data)
+
+def get_analytics(request):
+    if request.GET:
+        if not 'username' in request.GET:
+            return JSONResponse({
+                'error_code': 'E007',
+                'error': 'Please provide a valid username'
+            })
+        username = request.GET['username']
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return JSONResponse(
+                {
+                    'error': 'User does NOT exists.',
+                    'error_code': 'E001'
+                }
+            )
+        today_txns_objs = user.profile.get_today_transactions()
+        return JSONResponse({
+            "error_code": "F001",
+            "error": "Under Construction"
+        })
 
 class TransactionAPIView(APIView):
     def post(self, request, format=None):
